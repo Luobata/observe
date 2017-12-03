291d93347103c306bda5e0a32df681624b31c2b3
Luobata <602975805@qq.com>
2017-12-03 23:59:28 +0800

    add: test;
    Signed-off-by: Luobata <602975805@qq.com>

--- test/index.js
+++ test/index.js
@@ -1,21 +1,22 @@
-import { Observe } from '../src/index';
-
-const class wrap extends Observe {
-    constructor() {
-        console.log(this);
-        this.height = 1;
-    }
-};
-
-const class canvas extends Observe {
-    constructor() {
-        console.log(this);
-        //@observe;
-
-        this.$computed = {
-            height () {
-                return super.wrap.height + 1;
-            }
-        };
-    }
-}:
+console.log(1);
+//import { Observe } from '../src/index';
+//
+//const class wrap extends Observe {
+//    constructor() {
+//        console.log(this);
+//        this.height = 1;
+//    }
+//};
+//
+//const class canvas extends Observe {
+//    constructor() {
+//        console.log(this);
+//        //@observe;
+//
+//        this.$computed = {
+//            height () {
+//                return super.wrap.height + 1;
+//            }
+//        };
+//    }
+//}:
