6701d30facf38cd62b6c8919204d06c93c121e49
Luobata <602975805@qq.com>
2017-12-06 19:55:40 +0800

    init;
    Signed-off-by: Luobata <602975805@qq.com>

--- src/observe/index.js
+++ src/observe/index.js
@@ -1,6 +1,7 @@
 import { 
     isObj, 
     isFun,
+    has,
     noop,
 }  from 'UTIL/index';
 import Watcher from './watcher';
@@ -15,39 +16,38 @@ export class Observe {
         const name = target.constructor.name;
         this[name] = target;
 
-        if (has(target, $computed) && isObj(targe[$computed])) {
-            this.initComputed(target[$computed], name);
+        if (has(target, '$computed') && isObj(target['$computed'])) {
+            this.initComputed(target['$computed'], name);
         }
     };
 
     initComputed($computed: Object, name: String) {
         for (let i in $computed) {
             if (!has($computed, i)) continue;
-            if (!isFun($computed[i]) {
+            if (!isFun($computed[i])) {
                 new Error(`${this[name]} computed value ${i} is not a function`);
                 continue;
             }
+            $computed[i].call(this[name]);
+            Object.defineProperty(this[name], i, {
+                enumerable: true,
+                configurable: true,
+                get: (function get (fun, model) {
+                    const watcher = new Watcher(model, fun, noop);
+                    return function getter() {
+                        if (watcher.lazy) {
+                            watcher.evaluate();
+                        }
+                        if (Dep.target) {
+                            watcher.depend();
+                        }
+
+                        return watcher.value;
+                    };
+                })($computed[i], this[name]),
+                set: noop,
+            });
         }
-
-        $computed[i].call(this[name]);
-        Object.defineProperty(this[name], i, {
-            enumerable: true,
-            configurable: true,
-            get: (function get (fun, model) {
-                const watcher = new Watcher(model, fun, noop);
-                return function getter() {
-                    if (watcher.lazy) {
-                        watcher.evaluate();
-                    }
-                    if (Dep.target) {
-                        watcher.depend();
-                    }
-
-                    return watcher.value;
-                };
-            })($computed[i], this[name]),
-            set: noop,
-        });
     };
 };
 
